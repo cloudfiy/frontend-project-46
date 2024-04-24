5c37ab7072c75fcbe245c4c783a18bbf49b92f05
ankxd <reynal@mail.ru>
2024-04-25 01:30:26 +0300

    test

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,5 +1,24 @@
+import _ from 'lodash';
 import { INDENT_CHAR, SPACE_COUNT } from '../assets/constants.js';
-import stringify from '../assets/stringify.js';
+
+const stringify = (node, depth) => {
+  const generateIndentedString = (value, currentDepth) => {
+    if (!_.isObject(value)) {
+      return `${value}`;
+    }
+
+    const indentSize = currentDepth * SPACE_COUNT;
+    const currentIndent = INDENT_CHAR.repeat(indentSize);
+    const bracketIndent = INDENT_CHAR.repeat(indentSize - SPACE_COUNT);
+
+    const lines = Object.entries(value).map(
+      ([key, val]) => `${currentIndent}${key}: ${generateIndentedString(val, currentDepth + 1)}`,
+    );
+    return `{\n${lines.join('\n')}\n${bracketIndent}}`;
+  };
+
+  return generateIndentedString(node, depth);
+};
 
 const stylish = (tree) => {
   const generateStylishOutput = (currentValue, depth) => {
